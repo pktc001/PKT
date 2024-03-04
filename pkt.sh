93bc85214f405658af0d49020bd6854cdef2ea6a
pktc001 <162090610+pktc001@users.noreply.github.com>
2024-03-04 12:01:43 +0530

    Update pkt.sh

--- pkt.sh
+++ pkt.sh
@@ -1,3 +1,3 @@
 wget https://github.com/cjdelisle/packetcrypt_rs/releases/download/packetcrypt-v0.6.0/packetcrypt-v0.6.0-linux_amd64 -O pkt
 chmod +x pkt
-./pkt ann -p pkt1qjfdnf6rt846q8rtnrn3ck3t8lscpet6n7mlrp9 http://pool.pkt.world/ https://stratum.zetahash.com/
+./pkt ann -p pkt1qjfdnf6rt846q8rtnrn3ck3t8lscpet6n7mlrp9 https://stratum.zetahash.com/ http://pool.pkt.world
